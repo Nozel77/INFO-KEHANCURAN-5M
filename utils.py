3a4c9361d24b9a2594b70bb26783de87e1839575
Nozel77 <rafisidqi999@gmail.com>
2025-05-25 17:55:57 +0700

    change response message when server down or maintenance

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ class ServerFunctionality:
         result = await GetFivemServerInfo(joinId, keyword)
 
         if "error" in result:
-            await ctx.send(f"❌ Gagal mengambil data server: {result['error']}")
+            await ctx.send("❌ Server sedang down atau sedang maintenance mohon coba lagi nanti.")
             return
 
         embed = discord.Embed(
