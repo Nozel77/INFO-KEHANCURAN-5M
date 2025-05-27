7992e2f1f5f97fc50312cadb5c1f0488f279e755
Nozel77 <rafisidqi999@gmail.com>
2025-05-27 07:42:26 +0700

    delete asyncio

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-import asyncio
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
 from commands import Commands
 
-load_dotenv() 
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
 intents.members = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"✅ Bot {bot.user} siap digunakan.")
 
-async def main():
+def register_commands():
     Commands.info(bot)
     Commands.listkota(bot)
-    await bot.start(os.getenv("DISCORD_BOT_TOKEN"))
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("\n❌ Bot dihentikan oleh user")
+    register_commands()
+    token = os.getenv("DISCORD_BOT_TOKEN")
+    if not token:
+        print("❌ Token Discord BOT tidak ditemukan di environment variables!")
+        exit(1)
+    bot.run(token)
